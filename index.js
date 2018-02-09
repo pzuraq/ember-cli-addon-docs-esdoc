fcd45a80274b2dee868a863425d502426679346f
Christopher Garrett <me@pzuraq.com>
2018-02-08 22:33:12 -0800

    wip

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 'use strict';
 
+const ESDocCompiler = require('broccoli-esdoc');
+const ESDoc2JsonApi = require('./lib/esdoc-2-json-api');
+
+const ESDOC_PLUGINS = [
+  {
+    name: 'esdoc-ecmascript-proposal-plugin',
+    option: {
+      classProperties: true,
+      objectRestSpread: true,
+      doExpressions: true,
+      functionBind: true,
+      functionSent: true,
+      asyncGenerators: true,
+      decorators: true,
+      exportExtensions: true,
+      dynamicImport: true
+    }
+  },
+  { name: 'esdoc-accessor-plugin' }
+];
+
 module.exports = {
-  name: 'ember-cli-addon-docs-esdoc'
+  name: 'ember-cli-addon-docs-esdoc',
+
+  createDocsGenerator(inputPaths, { project, destDir }) {
+    let esdoc = new ESDocCompiler(inputPaths, {
+      plugins: ESDOC_PLUGINS,
+      dest: `./${destDir}`
+    });
+
+    return new ESDoc2JsonApi(esdoc, { project });
+  }
 };
